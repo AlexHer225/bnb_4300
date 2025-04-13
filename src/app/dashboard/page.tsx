c2417ac5d68225cc22ef70a65588f38b16692463
Evan Welborn <evanwelborn@gmail.com>
2025-04-13 18:43:00 -0400

    navbar on login, signup, and dashboard. Centered the hangry text in navbar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,10 @@
+import React from 'react';
+import Navbar from '../Navbar';
+
 export default function Dashboard() {
   return (
     <div>
+      <Navbar/>
       <h1>Dashboard</h1>
       <p>Welcome to your dashboard!</p>
     </div>
