b0079a58f942cc7a3f40743d3f286c49c7ef28ed
Evan Welborn <evanwelborn@gmail.com>
2025-04-22 14:33:42 -0400

    minor css changes

--- src/app/components/SignupLanding.tsx
+++ src/app/components/SignupLanding.tsx
@@ -6,7 +6,7 @@ export default function SignupLanding () {
     return (
         <div className="signup-landing-page">
             <h1>Thank you for signing up with Hangry!</h1>
-            <h2>Now please log into your new account</h2>
+            <h2>Now please <a href="/login">login</a> to your new account</h2>
         </div>
     )
 } 
